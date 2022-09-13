f03bf07d6dffc35e930ca04b7b87fedfdfea5846
Ashit Kumar Sinha <37330958+asitkr@users.noreply.github.com>
2022-09-13 21:07:33 +0530

    Add files via upload

---
+++ class 1/helper.js
@@ -0,0 +1,7 @@
+function helper() {
+    console.log("May I help you");
+}
+
+export  default helper;
+
+// module.exports = helper;
---
+++ class 1/index.js
@@ -0,0 +1,22 @@
+import helper from './helper.js';
+
+function callThisFunction() {
+    helper();
+    console.log("This is my first program of node js");
+}
+
+console.log("npm start");
+
+callThisFunction();
+
+
+// const helper = require('./helper');
+
+// helper();
+
+// // function kuchBhi() {
+// //     helper
+// //     console.log("This first Node js programm");
+// // }
+
+// // kuchBhi();
